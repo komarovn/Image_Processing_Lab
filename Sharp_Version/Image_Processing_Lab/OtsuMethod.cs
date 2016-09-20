b045d323e414be83d82bd852fa3fa6f420b9d282
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-09-20 20:30:00 +0300

    Reorganized repository

--- Sharp_Version/Image_Processing_Lab/OtsuMethod.cs
+++ Sharp_Version/Image_Processing_Lab/OtsuMethod.cs
@@ -12,6 +12,7 @@ namespace Image_Processing_Lab
     class OtsuMethod : ImageProcessing
     {
         private int[] intensityArray;
+        private int[] histogram;
 
         public OtsuMethod(Bitmap image) : base(image)
         {
@@ -34,5 +35,27 @@ namespace Image_Processing_Lab
                 for (int j = 0; j < sourceImage.Width; j++)
                     intensityArray[i * sourceImage.Width + j] = (int)(0.2126 * sourceImage.GetPixel(j, i).R + 0.7152 * sourceImage.GetPixel(j, i).G + 0.0722 * sourceImage.GetPixel(j, i).B);
         }
+
+        private void CalculateHistogram()
+        {
+            histogram = new int[256];
+            for (int i = 0; i < 256; i++)
+                histogram[i] = 0;
+            for (int i = 0; i < sourceImage.Height; i++)
+                for (int j = 0; j < sourceImage.Width; j++)
+                {
+                    int index = (int)(0.2126 * sourceImage.GetPixel(j, i).R + 0.7152 * sourceImage.GetPixel(j, i).G + 0.0722 * sourceImage.GetPixel(j, i).B);
+                    histogram[index]++;
+                }
+        }
+
+        public int MakeBinarized(int thereshold, int value)
+        {
+            if (thereshold > value)
+                return 0;
+            else
+                return 255;
+        }
+
     }
 }
