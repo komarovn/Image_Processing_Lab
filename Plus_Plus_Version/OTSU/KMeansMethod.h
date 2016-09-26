8396c71bde5e330d6d0db7d16ea9761bbde16ed1
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-09-26 23:01:37 +0300

    k-means

--- Plus_Plus_Version/OTSU/KMeansMethod.h
+++ Plus_Plus_Version/OTSU/KMeansMethod.h
@@ -13,14 +13,34 @@ using namespace System::Drawing;
 
 class KMeansMethod : public ImageProcessing
 {
+private:
+	int k;
+	int* imageGrid;
+	int* centresOfClusters;
 public:
 	Color CalculateNewPixelColor(int x, int y)
         {
             Color color = Color::Coral;
             return color;
         }
+
 	KMeansMethod(Bitmap ^image) : ImageProcessing(image)
 	{
-		// TODO: write here something
+		k = 2;													// число кластеров (задается пользователем в отдельном окне при вызове метода)
+		int sizeOfImage = image->Height * image->Width;			// размер изображения
+		imageGrid = new int[sizeOfImage];						// сетка, представляющая собой изображение
+		centresOfClusters = new int[k];							// центры k кластеров
+		for(int i = 0; i < k; i++)								// бросаем k точек на сетку случайным образом
+		{
+			int randomCell = rand() % (sizeOfImage);			// номер случайной клетки сетки
+			centresOfClusters[i] = imageGrid[randomCell];		// центр кластера
+		}
+
+	}
+
+	~KMeansMethod() 
+	{
+		delete [] imageGrid;
+		delete [] centresOfClusters;
 	}
 };
