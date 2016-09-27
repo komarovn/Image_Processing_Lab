201a54687dbf3f9c08b6d5953424b9eebef5a8e6
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-09-27 23:29:32 +0300

    SEHException occurs when medianFilter is executing

--- Plus_Plus_Version/OTSU/KMeansMethod.h
+++ Plus_Plus_Version/OTSU/KMeansMethod.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "ImageProcessing.h"
+#include <iostream>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/core/mat.hpp>
 
 #include <opencv\cv.h>
 
@@ -10,6 +14,7 @@ using namespace System::Collections;
 using namespace System::Windows::Forms;
 using namespace System::Data;
 using namespace System::Drawing;
+using namespace System::Drawing::Imaging;
 
 class KMeansMethod : public ImageProcessing
 {
@@ -26,8 +31,18 @@ public:
 
 	KMeansMethod(Bitmap ^image) : ImageProcessing(image)
 	{
+		BitmapData ^bmpData = sourceImage->LockBits(Rectangle(0, 0, sourceImage->Width, sourceImage->Height), Imaging::ImageLockMode::ReadWrite, sourceImage->PixelFormat);
+		cv::Mat src = cv::Mat::Mat(sourceImage->Height, sourceImage->Width, CV_8UC3, (void*)bmpData->Scan0, CV_AUTO_STEP);
+		cv::Mat dest = src;
+		
+		try {
+			cv::medianBlur(src, dest, 2);							// медианный фильтр для убирания шума, который влияет на метод k-средних
+		}
+		catch (System::Runtime::InteropServices::SEHException ^ex) {
+			;
+		}
 		k = 2;													// число кластеров (задается пользователем в отдельном окне при вызове метода)
-		int sizeOfImage = image->Height * image->Width;			// размер изображения
+		int sizeOfImage = sourceImage->Height * sourceImage->Width;			// размер изображения
 		imageGrid = new int[sizeOfImage];						// сетка, представляющая собой изображение
 		centresOfClusters = new int[k];							// центры k кластеров
 		for(int i = 0; i < k; i++)								// бросаем k точек на сетку случайным образом
@@ -36,6 +51,8 @@ public:
 			centresOfClusters[i] = imageGrid[randomCell];		// центр кластера
 		}
 
+		sourceImage->UnlockBits(bmpData);
+		Bitmap ^outputImage = gcnew Bitmap(dest.cols, dest.rows, 1, sourceImage->PixelFormat, (IntPtr)dest.data); // выходное изображение
 	}
 
 	~KMeansMethod() 
