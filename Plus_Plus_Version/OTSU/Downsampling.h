360b5eb81a7dd00ea06a840419a9c0623ae8b28a
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-10-17 22:45:25 +0300

    Processing new image with new height and width

--- Plus_Plus_Version/OTSU/Downsampling.h
+++ Plus_Plus_Version/OTSU/Downsampling.h
@@ -17,13 +17,33 @@ public:
 		newWidth = width;
 		newHeight = height;
 		image = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+		oldWidth = image.cols;
+		oldHeight = image.rows;
 		gaussianBlur();
+		subsample();
 	}
 
 	void gaussianBlur() 
 	{
 		cv::Mat dest = image;
 		cv::GaussianBlur(image, dest, cv::Size(13, 13), 0, 0);
+		image = dest;
+	}
+
+	void subsample()
+	{
+		cv::Mat dest = cv::Mat(newHeight, newWidth, CV_8UC3);
+		float aspectCoeffW = oldWidth / (float)newWidth;
+		float aspectCoeffH = oldHeight / (float)newHeight;
+		for(int i = 1; i < newWidth; i++)
+		{
+			for(int j = 1; j < newHeight; j++)
+			{
+				int y = j * aspectCoeffH - 1;				
+				int x = i * aspectCoeffW- 1;				
+				dest.at<cv::Vec3b>(j, i) = image.at<cv::Vec3b>(y, x);
+			}
+		}
 		image = dest;
 	}
 
