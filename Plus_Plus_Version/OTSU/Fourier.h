1c488190c8a3a91e6c748f38e97bc0fc75af0323
Nesterov Alexander <nesterov.a@ccam.cmc.unn.net>
2016-11-12 08:58:31 +0300

    Saturday work

--- Plus_Plus_Version/OTSU/Fourier.h
+++ Plus_Plus_Version/OTSU/Fourier.h
@@ -5,6 +5,8 @@
 #include <complex>
 #include <vector>
 
+#define PI 3.14159265
+
 class Fourier
 {
 private:
@@ -13,8 +15,6 @@ private:
 	int height;
 	int width;
 	vector<complex<double>> *intesnsityMatrix;
-	const double PI = 3.14159265;
-
 public:
 	Fourier(string filename)
 	{
@@ -29,7 +29,7 @@ public:
 		if (n == 1)  return;
 
 		vector<complex<double>> a0(n / 2), a1(n / 2);
-		for (int i = 0, j = 0; i<n; i += 2, ++j) {
+		for (int i = 0, j = 0; i < n; i += 2, ++j) {
 			a0[j] = a[i];
 			a1[j] = a[i + 1];
 		}
@@ -51,12 +51,13 @@ public:
 	{
 		double intensity;
 		int deg = static_cast<int>(log(width * height) / log(2.0)) + 1;
-		int leng = pow(2, deg);
+		int leng = Math::Pow(2, deg);
 		intesnsityMatrix = new vector<complex<double>>(leng);
 
-		for (int i = 0; i < leng; i++) (*intesnsityMatrix)[i] = 0;
+		for (int i = 0; i < leng; i++) 
+			(*intesnsityMatrix)[i] = 0;
 
-		int k = 0;
+		//int k = 0;
 		for (int x = 0; x < width; x++)
 			for (int y = 0; y < height; y++)
 			{
@@ -64,35 +65,55 @@ public:
 					0.2126 * sourceImage.at<cv::Vec3b>(y, x)[2] +
 					0.7152 * sourceImage.at<cv::Vec3b>(y, x)[1] +
 					0.0722 * sourceImage.at<cv::Vec3b>(y, x)[0];
-				(*intesnsityMatrix)[k] = intensity;
-				k++;
+				(*intesnsityMatrix)[y * width + x] = intensity;
+				//k++;
 			}
-
+		int k = width * height;
 		fft(*intesnsityMatrix, false);
 
-		Bitmap ^FI = gcnew Bitmap(width, height);
+		Bitmap ^outputFourierImage = gcnew Bitmap(width, height);
 		int col;
-		Color c;
+		Color color;
 
-		int max = static_cast<int>((*intesnsityMatrix)[0].real());
-		for (int i = 0; i < k; i++)
-			if (max < (*intesnsityMatrix)[i].real()) max = (*intesnsityMatrix)[i].real();
+		int max = static_cast<int>(Math::Sqrt(Math::Pow((*intesnsityMatrix)[0].real(), 2) +
+			Math::Pow((*intesnsityMatrix)[0].imag(), 2)));
+		int min = max;
+		int sqrtOfComplexNumber = 0;
+		for (int i = 0; i < k; i++) 
+		{
+			sqrtOfComplexNumber = static_cast<int>(Math::Sqrt(Math::Pow((*intesnsityMatrix)[i].real(), 2) +
+				Math::Pow((*intesnsityMatrix)[i].imag(), 2)));
+			if (max < sqrtOfComplexNumber)
+				max = sqrtOfComplexNumber;
+			if (min > sqrtOfComplexNumber)
+				min = sqrtOfComplexNumber;
+		}
 
-		k = 0;
+		int norm = 0;
+		//k = 0;
 		for (int i = 1; i < width; i++)
 		{
 			for (int j = 1; j < height; j++)
 			{
-				col = static_cast<int>((*intesnsityMatrix)[k].real());
-				if (col > 255) c = Color::FromArgb(255, 255, 255);
-				else if (col < 0) c = Color::FromArgb(0, 0, 0);
-				else c = Color::FromArgb(col, col, col);
-				FI->SetPixel(i, j, c);
-				k++;
+				col = static_cast<int>((Math::Sqrt(Math::Pow((*intesnsityMatrix)[j * width + i].real(), 2) +
+					Math::Pow((*intesnsityMatrix)[j * width + i].imag(), 2)) - min) / (double)(max - min) * 255);
+				if (col > 255)
+				{
+					color = Color::FromArgb(255, 255, 255);
+				}
+				else
+				{
+					if (col < 0)
+						color = Color::FromArgb(0, 0, 0);
+					else
+						color = Color::FromArgb(col, col, col);
+				}
+				outputFourierImage->SetPixel(i, j, color);
+				//k++;
 			}
 		}
 
-		return FI;
+		return outputFourierImage;
 	}
 
 	void InverseFourierTransform()
