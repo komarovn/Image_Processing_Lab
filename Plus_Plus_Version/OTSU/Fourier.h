34a9030063b826505a2868db6902c5a25574d31e
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-11-18 22:04:27 +0300

    Refactoring

--- Plus_Plus_Version/OTSU/Fourier.h
+++ Plus_Plus_Version/OTSU/Fourier.h
@@ -6,7 +6,7 @@
 #include <vector>
 #include <cmath>
 
-#define PI 3.14159265
+#define PI 3.1415926535
 
 class Fourier
 {
@@ -16,6 +16,8 @@ private:
 	int height;
 	int width;
 	vector<complex<double>> *intesnsityMatrix;
+	int *magnitudes;
+	int *phases;
 public:
 	Fourier(string filename)
 	{
@@ -48,62 +50,25 @@ public:
 		}
 	}
 
-	/*void DFT()
-	{
-		complex<double> I = sqrt(-1);
-		complex<double> value;
-		complex<double> coeff;
-		for(int k = 0; k < width; k++)
-		{
-			for(int l = 0; l < height; l++)
-			{
-				for(int i = 0; i < width; i++)
-				{
-					for(int j = 0; j < height; j++)
-					{
-						coeff = (-2 * PI * (k * i / width + l * j / height) * I);
-						value = intensityMatrixComplex[j * width + i] * coeff;
-					}
-				}
-				dftMatrix[l * width + k] = value;
-			}
-		}
-	} */
-
 	Bitmap^ FourierTransform()
 	{
-		double intensity;
 		int k = width * height;
 		int deg = static_cast<int>(log(width * height) / log(2.0)) + 1;
 		int leng = Math::Pow(2, deg);
 		intesnsityMatrix = new vector<complex<double>>(leng);
 
 		/* Задаем матрицу интенсивностей */
-		for (int x = 0; x < width; x++)
-			for (int y = 0; y < height; y++)
-			{
-				intensity =
-					0.2126 * sourceImage.at<cv::Vec3b>(y, x)[2] +
-					0.7152 * sourceImage.at<cv::Vec3b>(y, x)[1] +
-					0.0722 * sourceImage.at<cv::Vec3b>(y, x)[0];
-				(*intesnsityMatrix)[y * width + x] = complex<double>(intensity, 0);
-			}
-
-		/* Обнуляем оставшиеся элементы матрицы интенсивностей */
-		for (int i = k; i < leng; i++)
-			(*intesnsityMatrix)[i] = complex<double>(0, 0);
+		СalculateIntensityMatrix(leng);	
 	
 		/* Применяем быстрое преобразование Фурье */
 		fft(*intesnsityMatrix, false);
-		//DFT();
 
 		/* Формируем изображение образа преобразования */
 		Bitmap ^outputFourierImage = gcnew Bitmap(width, height);
 
 		/* Рассчитываем матрицы магнитуд и фаз */
-		int *magnitudes = new int[k];
-		int *magnitudes2 = new int[k];
-		int *phases = new int[k];
+		magnitudes = new int[k];
+		phases = new int[k];
 		float indexCoeff = leng / (float)k;
 		for (int i = 0; i < k; i++) 
 		{
@@ -139,6 +104,10 @@ public:
 		/* Считаем градацию серого и записываем цвет пикселя для изображения образа */
 		int grad;
 		Color color;
+		int halfWidth = width / 2;
+		int halfHeight = height / 2;
+		int m = 0;
+		int l = 0;
 		float coeff = 255 / (float)log(1 + max);
 		for (int i = 1; i < width; i++)
 		{
@@ -147,39 +116,44 @@ public:
 				//grad = static_cast<int>((Math::Sqrt(Math::Pow((*intesnsityMatrix)[j * width + i].real(), 2) +
 				//	Math::Pow((*intesnsityMatrix)[j * width + i].imag(), 2)) - min) / (double)(max - min) * 255);
 				grad = coeff * (float)log(abs(magnitudes[j * width + i]) + 1);
-				/*if (grad > 255)
-				{
-					color = Color::FromArgb(255, 255, 255);
-				}
+				color = Color::FromArgb(grad, grad, grad);
+				if(i + halfWidth <= width)
+					m = i + halfWidth - 1;
 				else
-				{
-					if (grad < 0)
-						color = Color::FromArgb(0, 0, 0);
-					else*/
-						color = Color::FromArgb(grad, grad, grad);
-				//}
-				int k = width / 2;
-				int l = height / 2;
-				if(i + k <= width)
-					k = i + k - 1;
+					m = i - halfWidth;
+				if(j + halfHeight <= height)
+					l = j + halfHeight - 1;
 				else
-					k = i - k;
-				if(l + j <= height)
-					l = j + l - 1;
-				else
-					l = j - l;
-				outputFourierImage->SetPixel(k, l, color);
+					l = j - halfHeight;
+				outputFourierImage->SetPixel(m, l, color);
 			}
 		}
 
-		delete [] magnitudes;
-		delete [] phases;
 		return outputFourierImage;
 	}
 
 	void InverseFourierTransform()
 	{
 		//fft(*intesnsityMatrix, true);
+	}
+
+	void СalculateIntensityMatrix(int leng)
+	{
+		double intensity;
+
+		for (int x = 0; x < width; x++)
+			for (int y = 0; y < height; y++)
+			{
+				intensity =
+					0.2126 * sourceImage.at<cv::Vec3b>(y, x)[2] +
+					0.7152 * sourceImage.at<cv::Vec3b>(y, x)[1] +
+					0.0722 * sourceImage.at<cv::Vec3b>(y, x)[0];
+				(*intesnsityMatrix)[y * width + x] = complex<double>(intensity, 0);
+			}
+
+		/* Обнуляем оставшиеся элементы матрицы интенсивностей */
+			for (int i = width * height; i < leng; i++)
+			(*intesnsityMatrix)[i] = complex<double>(0, 0);
 	}
 
 	Bitmap^ getImage()
@@ -203,7 +177,27 @@ public:
 		return outputImage;
 	}
 
-	~Fourier()
+	~Fourier() { }
+
+	/*void DFT()
 	{
-	}
+		complex<double> I = sqrt(-1);
+		complex<double> value;
+		complex<double> coeff;
+		for(int k = 0; k < width; k++)
+		{
+			for(int l = 0; l < height; l++)
+			{
+				for(int i = 0; i < width; i++)
+				{
+					for(int j = 0; j < height; j++)
+					{
+						coeff = (-2 * PI * (k * i / width + l * j / height) * I);
+						value = intensityMatrixComplex[j * width + i] * coeff;
+					}
+				}
+				dftMatrix[l * width + k] = value;
+			}
+		}
+	} */
 };
